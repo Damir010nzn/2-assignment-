839054fefbbd7660dfed0a7b7464bf2738b60ac6
Damir <252479@astanait.edu.kz>
2026-04-08 11:17:14 +0500

    destribute

---
+++ src/BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+    String accNo;
+    String name;
+    double balance;
+
+    public BankAccount(String accNo, String name, double balance) {
+        this.accNo = accNo;
+        this.name = name;
+        this.balance = balance;
+    }
+}
